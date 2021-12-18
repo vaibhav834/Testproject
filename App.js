ba0ea7a812967daf134e714ec6db1e91152e3b9c
vaibhav <vaibhav834.sharma@gmail.com>
2021-12-18 19:08:27 +0530

    project setup

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { Button, StyleSheet, View ,Text} from "react-native"
+import React, { useState } from "react"
+export default function App(){
+  const [value,setValue] = useState(0)
+  const pressed =()=>{
+    setValue(value+1)
+  }
+  return(
+    <>
+      <View style={styles.container}>
+        <Button title=" click me" onPress={pressed}/>
+        <Text>{value}</Text>
+      </View>
+    <View style={styles.container1}/>
+    <View style={styles.container2}/>
+    <View style={styles.container3}/>
+    </>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles =StyleSheet.create({
+  container:{
+    flex:0.5,
+    backgroundColor:"pink",
+    alignItems:"center",
+    justifyContent:"center"
   },
-});
+  container1:{
+    flex:0.2,
+    backgroundColor:"yellow"
+  },
+  container2:{
+    flex:0.2,
+    backgroundColor:"green"
+  },
+  container3:{
+    flex:0.1,
+    backgroundColor:"blue"
+  }
+})
